57403c7932b5fc6a433f0f45e284cac05fb861e0
duckdzung <laiducdung1911@gmail.com>
2025-05-19 10:50:04 +0700

    init pj back-end

---
+++ gscores-api/src/main/java/asia/goldenowl/gscores_api/GscoresApiApplication.java
@@ -0,0 +1,13 @@
+package asia.goldenowl.gscores_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GscoresApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GscoresApiApplication.class, args);
+	}
+
+}
---
+++ gscores-api/src/test/java/asia/goldenowl/gscores_api/GscoresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package asia.goldenowl.gscores_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GscoresApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
