40b5d41a9e083d588bf198410fcb9fe645455484
duckdzung <laiducdung1911@gmail.com>
2025-05-21 18:30:37 +0700

    add CORS configuration to WebConfig and remove CrossOrigin annotations from ReportController and ScoreController

--- gscores-api/src/main/java/asia/goldenowl/gscores_api/controller/ReportController.java
+++ gscores-api/src/main/java/asia/goldenowl/gscores_api/controller/ReportController.java
@@ -3,7 +3,6 @@ package asia.goldenowl.gscores_api.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/reports")
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ReportController {
 
