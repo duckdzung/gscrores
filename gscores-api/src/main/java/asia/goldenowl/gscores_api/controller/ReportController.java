4a34d4177cfefc2b23e97e83e8f97f2b4216aa63
duckdzung <laiducdung1911@gmail.com>
2025-05-21 12:12:40 +0700

    add CrossOrigin annotation to ReportController and ScoreController

--- gscores-api/src/main/java/asia/goldenowl/gscores_api/controller/ReportController.java
+++ gscores-api/src/main/java/asia/goldenowl/gscores_api/controller/ReportController.java
@@ -3,6 +3,7 @@ package asia.goldenowl.gscores_api.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/reports")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ReportController {
 
--- gscores-api/src/main/java/asia/goldenowl/gscores_api/controller/ScoreController.java
+++ gscores-api/src/main/java/asia/goldenowl/gscores_api/controller/ScoreController.java
@@ -2,6 +2,7 @@ package asia.goldenowl.gscores_api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/scores")
+@CrossOrigin(origins = "*")
 @Validated
 @RequiredArgsConstructor
 public class ScoreController {
